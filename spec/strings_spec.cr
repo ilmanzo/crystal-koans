6048b654a3a22ff88e37738e1864ad40f57d8b16
Andrea Manzini <ilmanzo@gmail.com>
2017-08-18 11:53:31 +0200

    more strings

--- spec/strings_spec.cr
+++ spec/strings_spec.cr
@@ -17,4 +17,18 @@ describe Student do
     hi += there
     hi.should eq FILL_IN_THIS_STRING
   end
+
+  it "interpolates values" do
+    value = 123
+    string = "The value is #{value}"
+    string.should eq FILL_IN_THIS_STRING
+  end
+
+  it "can extract substrings from a string" do
+    string = "Bacon, lettuce and tomato"
+    substr1 = string[7, 3]
+    substr2 = string[7..9]
+    substr1.should eq FILL_IN_THIS_STRING
+    substr2.should eq FILL_IN_THIS_STRING
+  end
 end
