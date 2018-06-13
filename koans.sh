a9ea062db6a788d50c9a0154c3388727a7a49cd1
Andrea Manzini <ilmanzo@gmail.com>
2018-06-13 22:07:00 +0200

    classes

--- koans.sh
+++ koans.sh
@@ -2,7 +2,7 @@
 #uncomment for debugging
 #set -x
 #set the order of tests
-tests=(bool numbers strings symbols arrays tuples hashes ranges loops structs classes)
+tests=(booleans numbers strings symbols arrays tuples hashes ranges loops structs classes)
 #
 for t in "${tests[@]}" 
 do
--- spec/classes_spec.cr
+++ spec/classes_spec.cr
@@ -3,11 +3,26 @@ require "./spec_helper"
 class FirstClass
 end
 
+class Dog1
+  def set_name(a_name)
+    @name=a_name
+  end
+
+  def get_name
+    @name
+  end
+end
+
 
 describe Student do
   it "can create instances of classes" do
     instance=FirstClass.new
     instance.class.should eq FILL_IN_THIS
+  end
+
+  it "can assign and retrieve instance variables" do
+    dog=Dog.new("Fido")
+    dog.name.should eq FILL_IN_THIS   # accessing instance variables require always a method
   end
 
 end
