d64cc5a8422b2d61a685a62a90f1c1a13327eb8a
Andrea Manzini <ilmanzo@gmail.com>
2018-06-19 12:05:45 +0200

    Update nil_spec.cr
    fixes #2

--- spec/nil_spec.cr
+++ spec/nil_spec.cr
@@ -3,8 +3,8 @@ require "./spec_helper"
 describe Student do
   # The Nil type has only one possible value: nil.
   it "knows nil is an object" do
-    Nil.class.should eq FILL_IN_THIS
-    nil.class.should eq FILL_IN_THIS 
+    Nil.class.to_s.should eq FILL_IN_THIS
+    nil.class.to_s.should eq FILL_IN_THIS 
   end
 
   it "knows nil's methods" do
