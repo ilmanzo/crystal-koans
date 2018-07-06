cf90484032fab6ce53746bb6a3b036d143d4d002
Andrea Manzini <ilmanzo@gmail.com>
2018-07-06 14:10:10 +0200

    func

--- spec/blocks_spec.cr
+++ spec/blocks_spec.cr
@@ -2,5 +2,11 @@ require "./spec_helper"
 
 describe Student do 
     
+  it "knows yield" do
+  end
+
+  it "knows blocks passing" do
+  end
+  
 end
 
--- spec/functions_spec.cr
+++ spec/functions_spec.cr
@@ -8,6 +8,17 @@ def double(x : Int)
   x*2
 end
 
+def manyargs(*args) #*args is a "splat" 
+  return args.size
+end
+
+## double splat captures named parameters
+def grocerylist(amount, **what) 
+  return amount+what.size
+end
+
+
+
 describe Student do
   it "knows functions" do
     value=dummyfunc # function invocations returns a value
@@ -21,12 +32,16 @@ describe Student do
     another.should eq FILL_IN_THIS
   end
 
-  it "knows yield" do
-  end
-
-  it "knows blocks passing" do
-  end
 
   it "knows splat parameters" do
+    value=manyargs("you","can","call","me","with","many","parameters")
+    value.should eq FILL_IN_THIS
   end
+
+  it "knows double splat parameters" do
+    value=grocerylist(amount: 5, bananas: 3, apples: 2, oranges: 4 )
+    value.should eq FILL_IN_THIS
+  end
+
+
 end
