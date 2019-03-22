60f9e0ce377b6d4eae65ec2bbec26b17f139af51
Andrea Manzini <ilmanzo@gmail.com>
2019-03-22 11:11:57 +0100

    blocks

--- spec/blocks_spec.cr
+++ spec/blocks_spec.cr
@@ -1,12 +1,31 @@
 require "./spec_helper"
 
+def method_with_block
+  result = yield
+  result
+end
+
+def method_with_block_arguments
+  yield("Bacon")
+end
+
+
 describe Student do 
     
   it "knows yield" do
+    #blocks can be curly braces delimited 
+    first_result = method_with_block { 1 + 2 }
+    first_result.should eq FILL_IN_THIS
+    #or also with do .. end
+    second_result = method_with_block do 3+4 end
+    second_result.should eq FILL_IN_THIS
   end
 
-  it "knows blocks passing" do
+  it "knows block argument passing" do
+    method_with_block_arguments do |argument| 
+      argument.should eq FILL_IN_THIS
+    end
   end
-  
+
 end
 
--- spec/regular_expressions_spec.cr
+++ spec/regular_expressions_spec.cr
@@ -1,5 +1,7 @@
 require "./spec_helper"
 
+#TODO
+
 describe Student do
   it "knows regular expressions" do
   end
