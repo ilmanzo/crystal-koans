b23762c4b1f90efd652d582f8359877cd6c3de5f
Andrea Manzini <ilmanzo@gmail.com>
2020-03-25 17:06:07 +0100

    exceptions

---
+++ spec/exceptions_spec.cr
@@ -0,0 +1,24 @@
+require "./spec_helper"
+
+class MyError < Exception
+end
+
+def failing_function(x)
+  raise MyError.new("OH NO!") if x == 10
+  raise "OH NO!" if x < 5
+  x
+end
+
+describe Student do
+  it "knows exception handling" do
+    # # implement code to handle this possible failure
+    n = failing_function(3)
+  end
+
+  it "knows custom exceptions" do
+    expect_raises(MyError) do
+      x = FILL_IN_THIS_NUMBER
+      n = failing_function(x)
+    end
+  end
+end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,6 +1,7 @@
 require "spec"
 
 FILL_IN_THIS = nil
+FILL_IN_THIS_NUMBER = 42
 
 class Student
 end
