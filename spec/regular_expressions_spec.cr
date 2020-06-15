5f05ed008707bb088ed6536ef9909bf36ae31015
Dorian Marié <dorian.marie.france@gmail.com>
2020-06-15 22:44:45 +0200

    add regexp tests

--- spec/regular_expressions_spec.cr
+++ spec/regular_expressions_spec.cr
@@ -1,9 +1,15 @@
 require "./spec_helper"
 
-#TODO
-
 describe Student do
   it "knows regular expressions" do
+    (/hello/ =~ "hello world").should eq FILL_IN_THIS
   end
 
+  it "returns match data" do
+    /hello/.match("hello world").class.should eq FILL_IN_THIS
+  end
+
+  it "knows about regular expressions's groups" do
+    /([a-z]+) ([a-z]+)/.match("hello world").try(&.[2]).should eq FILL_IN_THIS
+  end
 end
