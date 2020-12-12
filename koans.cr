7b8787912f67f5f7764a13ea669de73541b87942
Andrea Manzini <ilmanzo@gmail.com>
2020-12-12 11:27:22 +0100

    added some colors and emoji to koans runner

--- koans.cr
+++ koans.cr
@@ -1,3 +1,5 @@
+require "colorize"
+
 TESTS = %w(
   booleans
   numbers
@@ -41,9 +43,10 @@ TESTS.each_with_index(1) do |test_case, test_number|
   puts "Level #{test_number}: testing your strength on #{test_case} ..."
   spec_process = Process.run("crystal spec spec/#{test_case}_spec.cr", shell: true, error: STDERR, output: STDOUT)
   unless spec_process.success?
-    puts "The Master says: "
-    puts "Something is wrong. Please meditate on #{test_case} topic "
-    puts "and try to follow the path to Enlightenment"
+    test_case_bold=test_case.colorize(:green).mode(:bold)
+    print "--- \u{1F9D9} The Master says: ---\n".colorize(:yellow)
+    print "Something is wrong. Please meditate on ".colorize(:green)
+    print test_case_bold," topic\nand try to follow the path to Enlightenment \u{1F9D8}\n".colorize(:green)
     exit 1
   end
 end
